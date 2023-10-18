6efb0027d320e61a532b3c7310843fcd51e6948b
Michael R. Wolf <MichaelRWolf@att.net>
2023-10-18 18:43:45 -0400

    ^ f Risk=risky displays '!'

--- verify_all.sh
+++ verify_all.sh
@@ -7,5 +7,6 @@ function verify_fn(){
 }
 
 echo "Hello world" |  verify_fn -thello-world
-ricm -r safe -i refactoring 'Randomness' | verify_fn -tsafe_refactoring
+ricm -r safe -i refactoring 'Randomness'     | verify_fn -tsafe_refactoring
 ricm -r validated -i refactoring 'Message 2' | verify_fn -tvalidated_refactoring
+ricm -r risky -i refactoring 'Message 3'     | verify_fn -trisky_refactoring
